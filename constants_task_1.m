2b1e79ed9cd205b1596cfde634faf0a95c6278a1
M S Harihara Sudhan <hariharasudhanms12@gmail.com>
2025-02-05 21:24:23 +0530

    Add files via upload

---
+++ constants_task_1.m
@@ -0,0 +1,15 @@
+%motor parameters
+
+La = 0.032 % armature inductance
+Kt = 0.8 % torque constant
+J = 0.3 % rotor inertia
+Ra = 0.02 % armature resistance
+B = 0.005 % damping coefficient of motor
+Kb = 0.8 % back emf constant
+M = 1200 % mass of boat
+R = 0.6 % propeller radius
+A = pi*R*R % propeller area
+p = 1000 % density of water
+%k = 0.7 % propeller pitch 
+D = 75 % drag coefficient
+k = 0.063 % propeller thrust coefficient
